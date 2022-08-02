9b2fbaafbff7abd55fda314b23d8cbf4ec5f16bb
Athul-Krishna <athulkrishnaks1998@gmail.com>
2022-08-02 16:28:08 +0530

    Add createUser endpoint

--- UserService/src/main/java/com/reboot/servo/controllers/UsersController.java
+++ UserService/src/main/java/com/reboot/servo/controllers/UsersController.java
@@ -1,10 +1,24 @@
 package com.reboot.servo.controllers;
 
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.reboot.servo.models.CreateUserRequestModel;
+import com.reboot.servo.models.CreateUserResponseModel;
+import com.reboot.servo.services.UsersService;
+import com.reboot.servo.shared.UserDto;
 
 @RestController
 @RequestMapping("/users")
@@ -13,8 +27,22 @@ public class UsersController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    UsersService usersService;
+
     @GetMapping("/status")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
+        produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        UserDto createdUser = usersService.createUser(userDto);
+        CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
 }
